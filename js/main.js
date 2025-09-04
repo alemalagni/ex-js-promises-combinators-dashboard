203acd40f9c4da3f729c56f8f28a5829169a89df
alemalagni <alemalagni@gmail.com>
2025-09-04 13:59:18 +0200

    feat: add weather

--- js/main.js
+++ js/main.js
@@ -6,10 +6,14 @@ async function fetchJson(url) {
 
 const getDashboardData = async (query) => {
     const city = await fetchJson(`http://localhost:3333/destinations?search=${query}`)
+    const weather = await fetchJson(`http://localhost:3333/weathers?search=${query}`)
 
     console.log(`
         Nome completo: ${city[0].name}
-        Paese: ${city[0].country}`
+        Paese: ${city[0].country}
+        Temperatura: ${weather[0].temperature}°C
+        Temperatura: ${weather[0].weather_description}
+        `
     );
 }
 
