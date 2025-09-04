b35ca27f81f6a42bec4630ee81429e63e6a9f409
alemalagni <alemalagni@gmail.com>
2025-09-04 14:05:59 +0200

    feat: add airport

--- js/main.js
+++ js/main.js
@@ -7,8 +7,9 @@ async function fetchJson(url) {
 const getDashboardData = async (query) => {
     const city = fetchJson(`http://localhost:3333/destinations?search=${query}`)
     const weather = fetchJson(`http://localhost:3333/weathers?search=${query}`)
+    const airport = fetchJson(`http://localhost:3333/airports?search=${query}`)
 
-    const dashboard = await Promise.all([city, weather])
+    const dashboard = await Promise.all([city, weather, airport])
 
     console.log(dashboard)
 
@@ -16,7 +17,8 @@ const getDashboardData = async (query) => {
         Nome completo: ${dashboard[0][0].name}
         Paese: ${dashboard[0][0].country}
         Temperatura: ${dashboard[1][0].temperature}°C
-        Temperatura: ${dashboard[1][0].weather_description}
+        Meteo: ${dashboard[1][0].weather_description}
+        Aeroporto: ${dashboard[2][0].name}
         `
     );
 }
